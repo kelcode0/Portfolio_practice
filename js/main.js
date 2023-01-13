ca4bb0106097338e9ca8c08b4f3fa79678e80872
kelcode0 <kelvinortiz@gmail.com>
2023-01-13 00:16:43 -0300

    comenzando con el main

--- js/main.js
+++ js/main.js
@@ -12,6 +12,25 @@ if(navToggle){
     })
 };
 
+/* menu hiden  */
+/* validate if constat exists*/
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu');
+    })
+};
+
+//Remove menu mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
 
 
 
